ca284ba9d6dfb33eb1bb4c2ed828269d5d24399f
GZC201314 <1739084007@qq.com>
2021-09-07 22:21:33 +0800

    338. 比特位计数

--- src/main/java/org/gzc/leetcode/SolutionHead.java
+++ src/main/java/org/gzc/leetcode/SolutionHead.java
@@ -1933,7 +1933,7 @@ class SolutionHead {
   }
 
   public static int bulbSwitch(int n) {
-    return (int) (Math.ceil(Math.sqrt(n+1))-1);
+    return (int) (Math.ceil(Math.sqrt(n + 1)) - 1);
   }
 
   public int[] maxNumber(int[] nums1, int[] nums2, int k) {
@@ -1992,6 +1992,42 @@ class SolutionHead {
     return compare(nums1, p1 + 1, nums2, p2 + 1);
   }
 
+  /**
+   * 338. 比特位计数
+   *
+   * <p>给你一个整数 n ，对于 0 <= i <= n 中的每个 i ，计算其二进制表示中 1 的个数 ，
+   *
+   * <p>返回一个长度为 n + 1 的数组 ans 作为答案。
+   */
+  public int[] countBits(int n) {
+    int[] bits = new int[n + 1];
+    for (int i = 0; i <= n; i++) {
+      bits[i] = countOnes(i);
+    }
+    return bits;
+  }
+
+  public int[] countBits1(int n) {
+    int[] result = new int[n + 1];
+    result[0] = 0;
+    for (int i = 0; i <= n; i++) {
+      if (i % 2 == 0) {
+        result[i] = result[i / 2];
+      } else {
+        result[i] = result[i - 1] + 1;
+      }
+    }
+    return result;
+  }
+
+  private int countOnes(int i) {
+    int ones = 0;
+    while (i > 0) {
+      i &= (i - 1);
+      ones++;
+    }
+    return ones;
+  }
 
   public static void main(String[] args) {
     char[][] matrix =
