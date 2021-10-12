d9b9fcf48b315a133bc54d00504e4144b047928d
GZC201314 <1739084007@qq.com>
2021-10-12 22:01:27 +0800

    337. 打家劫舍 III

--- src/main/java/org/gzc/leetcode/SolutionHead.java
+++ src/main/java/org/gzc/leetcode/SolutionHead.java
@@ -1,6 +1,5 @@
 package org.gzc.leetcode;
 
-import com.sun.org.apache.regexp.internal.RE;
 import org.gzc.leetcode.model.ListNode;
 import org.gzc.leetcode.model.TreeNode;
 import org.gzc.leetcode.model.UnionFind;
@@ -52,6 +51,32 @@ class SolutionHead {
   Map<String, PriorityQueue<String>> findItineraryMap = new HashMap<>();
   List<String> findItineraryResult = new LinkedList<>();
   /**
+   * 336. 回文对
+   *
+   * <p>给定一组 互不相同 的单词， 找出所有 不同 的索引对 (i, j)，使得列表中的两个单词， words[i] + words[j] ，
+   *
+   * <p>可拼接成回文串。
+   */
+  List<String> wordRev = new ArrayList<>();
+
+  Map<String, Integer> indices = new HashMap<>();
+  /**
+   * 337. 打家劫舍 III
+   *
+   * <p>在上次打劫完一条街道之后和一圈房屋后，小偷又发现了一个新的可行窃的地区。这个地区只有一个入口，我们称之为“根”。
+   * 除了“根”之外，每栋房子有且只有一个“父“房子与之相连。一番侦察之后，聪明的小偷意识到“这个地方的所有房屋的排列类似于一棵二叉树”。
+   * 如果两个直接相连的房子在同一天晚上被打劫，房屋将自动报警。
+   *
+   * <p>算法 动态规划 f(o) 表示选择 o 节点的情况下，o 节点的子树上被选择的节点的最大权值和；g(o)表示不选择 o 节点的情况下，o 节点的子树上被选择的节点的最大权值和；l 和
+   * r 代表 o 的左右孩子。
+   *
+   * <p>当 o 被选中时，o 的左右孩子都不能被选中，故 oo 被选中情况下子树上被选中点的最大权值和为 ll 和 rr 不被选中的最大权值和相加，即 f(o)=g(l)+g(r)。 当 o
+   * 不被选中时，o 的左右孩子可以被选中，也可以不被选中。对于 o 的某个具体的孩子 x，它对 o 的贡献是 x 被选中和不被选中情况下权值和的较大值。故 g(o)
+   * =max{f(l),g(l)}+max{f(r),g(r)}。
+   */
+  Map<TreeNode, Integer> f = new HashMap<>();
+  Map<TreeNode, Integer> g = new HashMap<>();
+  /**
    * 301. 删除无效的括号
    *
    * <p>给你一个由若干括号和字母组成的字符串 s ，删除最小数量的无效括号，使得输入的字符串有效。
@@ -59,7 +84,6 @@ class SolutionHead {
    * <p>返回所有可能的结果。答案可以按 任意顺序 返回。
    */
   private int len;
-
   private char[] charArray;
 
   /**
@@ -2543,29 +2567,20 @@ class SolutionHead {
     return patches;
   }
 
-  /**
-   * 336. 回文对
-   *
-   * <p>给定一组 互不相同 的单词， 找出所有 不同 的索引对 (i, j)，使得列表中的两个单词， words[i] + words[j] ，
-   *
-   * <p>可拼接成回文串。
-   */
-  List<String> wordRev = new ArrayList<>();
-  Map<String, Integer> indices = new HashMap<>();
   public List<List<Integer>> palindromePairs(String[] words) {
     int n = words.length;
     for (String word : words) {
-        wordRev.add(new StringBuffer(word).reverse().toString());
+      wordRev.add(new StringBuffer(word).reverse().toString());
     }
     for (int i = 0; i < n; i++) {
-      indices.put(wordRev.get(i),i);
+      indices.put(wordRev.get(i), i);
     }
 
     List<List<Integer>> ret = new ArrayList<>();
-    for (int i = 0; i <n; i++) {
+    for (int i = 0; i < n; i++) {
       String word = words[i];
       int m = words[i].length();
-      if(m == 0){
+      if (m == 0) {
         continue;
       }
       for (int j = 0; j <= m; j++) {
@@ -2586,18 +2601,36 @@ class SolutionHead {
     return ret;
   }
 
-
   private int findWord(String word, int left, int right) {
-    return indices.getOrDefault(word.substring(left,right+1),-1);
+    return indices.getOrDefault(word.substring(left, right + 1), -1);
   }
 
   private boolean isPalindrome(String word, int left, int right) {
-    int len = right-left+1;
-    for(int i = 0; i < len/2; i++) {
-      if(word.charAt(left+i)!=word.charAt(right-i)){
+    int len = right - left + 1;
+    for (int i = 0; i < len / 2; i++) {
+      if (word.charAt(left + i) != word.charAt(right - i)) {
         return false;
       }
     }
     return true;
+  }
+
+  public int rob(TreeNode root) {
+    dfs_rob(root);
+    return Math.max(f.getOrDefault(root, 0), g.getOrDefault(root, 0));
+  }
+
+  private void dfs_rob(TreeNode root) {
+    if (root == null) {
+      return;
+    }
+    dfs_rob(root.left);
+    dfs_rob(root.right);
+    // 如果根节点被选中,则左右子节点不被选中
+    f.put(root, root.val + g.getOrDefault(root.left, 0) + g.getOrDefault(root.right, 0));
+    g.put(
+        root,
+        Math.max(f.getOrDefault(root.left, 0), g.getOrDefault(root.left, 0))
+            + Math.max(f.getOrDefault(root.right, 0), g.getOrDefault(root.right, 0)));
   }
 }
--- src/main/java/org/gzc/leetcode/model/TreeNode.java
+++ src/main/java/org/gzc/leetcode/model/TreeNode.java
@@ -1,11 +1,30 @@
 package org.gzc.leetcode.model;
 
+import java.util.Objects;
+
 public class TreeNode {
     public int val;
     public TreeNode left;
     public TreeNode right;
 
     public TreeNode() {
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        TreeNode treeNode = (TreeNode) o;
+        return val == treeNode.val && left.equals(treeNode.left) && right.equals(treeNode.right);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(val, left, right);
     }
 
     public TreeNode(int val) {
