8d0be4052ac4b5edb8f0209a0447f5c91b6e353f
GZC201314 <1739084007@qq.com>
2021-11-13 22:13:40 +0800

    357. 计算各个位数不同的数字个数 520. 检测大写字母

--- src/main/java/org/gzc/leetcode/Solution11.java
+++ src/main/java/org/gzc/leetcode/Solution11.java
@@ -8,6 +8,31 @@ import java.util.*;
  */
 public class Solution11 {
     /**
+     * 495. 提莫攻击
+     */
+    public static int findPoisonedDuration(int[] timeSeries, int duration) {
+        int prisonTimeStart = 0;
+        int prisonTimeEnd = 0;
+        int prisonSum = 0;
+        for (Integer timeSerie : timeSeries) {
+            if (prisonTimeEnd <= timeSerie) {
+                prisonSum += (prisonTimeEnd - prisonTimeStart);
+                prisonTimeStart = timeSerie;
+                prisonTimeEnd = timeSerie + duration;
+            } else if (prisonTimeEnd < (timeSerie + duration)) {
+                prisonTimeEnd = timeSerie + duration;
+            }
+        }
+        prisonSum += (prisonTimeEnd - prisonTimeStart);
+        return prisonSum;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = new int[]{1, 4};
+        System.out.println(findPoisonedDuration(arr, 2));
+    }
+
+    /**
      * 343. 整数拆分
      * <p>
      * 算法 动态规划
@@ -31,7 +56,7 @@ public class Solution11 {
 
     /**
      * 347. 前K个高频元素
-     *
+     * <p>
      * map 加 桶排序
      */
     public int[] topKFrequent(int[] nums, int k) {
@@ -66,39 +91,64 @@ public class Solution11 {
             if (list[i] == null) {
                 continue;
             }
-            if(res.size()+list[i].size()<=k){
+            if (res.size() + list[i].size() <= k) {
                 res.addAll(list[i]);
-            }else{
+            } else {
                 int size = k - res.size();
 
-                res.addAll(list[i].subList(0,size));
+                res.addAll(list[i].subList(0, size));
             }
         }
         return res.stream().mapToInt(Integer::valueOf).toArray();
     }
 
     /**
-     * 495. 提莫攻击
+     * 520. 检测大写字母
      */
-    public static int findPoisonedDuration(int[] timeSeries, int duration) {
-        int prisonTimeStart = 0;
-        int prisonTimeEnd = 0;
-        int prisonSum = 0;
-        for (Integer timeSerie : timeSeries) {
-            if (prisonTimeEnd <= timeSerie) {
-                prisonSum += (prisonTimeEnd - prisonTimeStart);
-                prisonTimeStart = timeSerie;
-                prisonTimeEnd = timeSerie + duration;
-            } else if (prisonTimeEnd < (timeSerie + duration)) {
-                prisonTimeEnd = timeSerie + duration;
+    public boolean detectCapitalUse(String word) {
+        //如果所有的字母都是大写字母
+        if (word.toUpperCase().equals(word) || word.toLowerCase().equals(word)) {
+            return true;
+        }
+        //如果首字母是大写
+        if ('A' <= word.charAt(0) && word.charAt(0) <= 'Z') {
+            String substring = word.substring(1);
+            if (substring.toLowerCase().equals(substring)) {
+                return true;
             }
         }
-        prisonSum +=(prisonTimeEnd-prisonTimeStart);
-        return prisonSum;
+        return false;
     }
 
-    public static void main(String[] args) {
-        int[] arr = new int[]{1,4};
-        System.out.println(findPoisonedDuration(arr, 2));
+    /**
+     * 357. 计算各个位数不同的数字个数
+     * <p>
+     * 算法思路 动态规划
+     * <p>
+     * 比如，个位数中各个位数不同的数字个数为9（这里0不算个位数），
+     * <p>
+     * 那么两位数中各个位数不同的数字就是在这9个数的后面插入另一个不同的数字，
+     * <p>
+     * 所以个数为9 * 9 = 81。可能有人问，为什么只在后面插，不在前面插，这是因为会重复，
+     * <p>
+     * 比如1后插2等于2前面插1。而在1前面插0就不是两位数了，已经算在了个位数里。
+     * <p>
+     * <p>
+     * 转移方程:dp[i] = dp[i-1]*(11-i);
+     */
+    public int countNumbersWithUniqueDigits(int n) {
+        int[] dp = new int[n + 1];
+        if (n == 0) {
+            return 1;
+        }
+        dp[0] = 1;
+        dp[1] = 9;
+        int sum = dp[0] + dp[1];
+        for (int i = 2; i <= n; i++) {
+            dp[i] = dp[i - 1] * (11 - i);
+            sum += dp[i];
+        }
+
+        return sum;
     }
 }
