81d6825b45bdf5a3154e5f82393ced15ac7b7280
GZC201314 <1739084007@qq.com>
2021-12-02 23:20:37 +0800

    506. 相对名次

--- src/main/java/org/gzc/leetcode/Solution12.java
+++ src/main/java/org/gzc/leetcode/Solution12.java
@@ -1,5 +1,9 @@
 package org.gzc.leetcode;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.PriorityQueue;
+
 /**
  * @author GZC
  * @description 十二月份的LeetCode练习代码
@@ -32,32 +36,64 @@ public class Solution12 {
 
     public static int superPow(int a, int[] b) {
         int len = b.length;
-        return indexPow(a,b,len);
+        return indexPow(a, b, len);
     }
 
     private static int indexPow(int a, int[] b, int len) {
-        if(len <1){
+        if (len < 1) {
             return 1;
         }
-        int part1 = myPow(a,b[len-1]);
+        int part1 = myPow(a, b[len - 1]);
         len--;
-        int part2 = myPow(indexPow(a,b,len),10);
+        int part2 = myPow(indexPow(a, b, len), 10);
 
-        return part1*part2%1337;
+        return part1 * part2 % 1337;
     }
 
     /*防止溢出*/
     private static int myPow(int a, int k) {
-        a = a%1337;
-        int ans =1;
-        for (int i=0;i<k;i++){
-            ans *=a;
-            ans %=1337;
+        a = a % 1337;
+        int ans = 1;
+        for (int i = 0; i < k; i++) {
+            ans *= a;
+            ans %= 1337;
         }
         return ans;
     }
 
+    public static String[] findRelativeRanks(int[] score) {
+        int n = score.length;
+        if (n == 1) {
+            return new String[]{"Gold Medal"};
+        }
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < n; ++i) {
+            map.put(score[i], i);
+        }
+        PriorityQueue<Map.Entry<Integer, Integer>> pq = new PriorityQueue<>((a, b) -> b.getKey() - a.getKey());
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            pq.offer(entry);
+        }
+        int rank = 1;
+        String[] res = new String[n];
+        while (!pq.isEmpty()) {
+            Map.Entry<Integer, Integer> entry = pq.poll();
+            int idx = entry.getValue();
+            if (rank == 1) {
+                res[idx] = "Gold Medal";
+            } else if (rank == 2) {
+                res[idx] = "Silver Medal";
+            } else if (rank == 3) {
+                res[idx] = "Bronze Medal";
+            } else {
+                res[idx] = "" + rank;
+            }
+            rank++;
+        }
+        return res;
+    }
+
     public static void main(String[] args) {
-        System.out.println(superPow(2147483647 ,new int[]{2,0,0}));
+        System.out.println(superPow(2147483647, new int[]{2, 0, 0}));
     }
 }
