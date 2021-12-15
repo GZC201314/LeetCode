73f11439a6690c110a03b284d0bc8a8e6a2c2db9
GZC201314 <1739084007@qq.com>
2021-12-15 22:11:36 +0800

    387. 字符串中的第一个唯一字符

--- src/main/java/org/gzc/leetcode/Solution12.java
+++ src/main/java/org/gzc/leetcode/Solution12.java
@@ -219,4 +219,29 @@ public class Solution12 {
         return Math.max(dp[i-1][0],dp[i-1][1])+1;
     }
 
+    public int firstUniqChar(String s) {
+        for (int i = 0; i < s.length(); i++) {
+
+            String newString = s.replaceFirst(String.valueOf(s.charAt(i)), "");
+            if(!newString.contains(String.valueOf(s.charAt(i)))){
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public int firstUniqChar1(String s) {
+        Map<Character, Integer> frequency = new HashMap<Character, Integer>();
+        for (int i = 0; i < s.length(); ++i) {
+            char ch = s.charAt(i);
+            frequency.put(ch, frequency.getOrDefault(ch, 0) + 1);
+        }
+        for (int i = 0; i < s.length(); ++i) {
+            if (frequency.get(s.charAt(i)) == 1) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
 }
