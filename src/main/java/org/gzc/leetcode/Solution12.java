2238c6b2e76326f5bbf5c441ab665ed565481158
GZC201314 <1739084007@qq.com>
2021-12-28 22:45:37 +0800

    377. 组合总数4

--- src/main/java/org/gzc/leetcode/Solution12.java
+++ src/main/java/org/gzc/leetcode/Solution12.java
@@ -341,4 +341,20 @@ public class Solution12 {
         return result;
     }
 
+    /**
+     * 377. 组合总数4
+     */
+    public int combinationSum4(int[] nums, int target) {
+        int[] dp = new int[target + 1];
+        dp[0] = 1;
+        for (int i = 1; i <= target; i++) {
+            for (int num : nums) {
+                if (num <= i) {
+                    dp[i] += dp[i - num];
+                }
+            }
+        }
+        return dp[target];
+    }
+
 }
