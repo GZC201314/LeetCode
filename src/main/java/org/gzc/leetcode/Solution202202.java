d87b277476331863379bef3a2911b30ab899101c
GZC201314 <1739084007@qq.com>
2022-02-14 21:37:11 +0800

    540. 有序数组中的单一元素（情人节的每日一题,伤害性不大，侮辱性极强）

--- src/main/java/org/gzc/leetcode/Solution202202.java
+++ src/main/java/org/gzc/leetcode/Solution202202.java
@@ -99,4 +99,22 @@ public class Solution202202 {
     int getIdx(int x, int y) {
         return x * n + y + 1;
     }
+
+    /**
+     * 540. 有序数组中的单一元素
+     * @param nums
+     * @return
+     */
+    public int singleNonDuplicate(int[] nums) {
+        int low =0,high = nums.length-1;
+        while (low<high){
+            int mid = (high-low)/2+low;
+            if(nums[mid] == nums[mid^1]){
+                low = mid+1;
+            }else {
+                high = mid;
+            }
+        }
+        return nums[low];
+    }
 }
