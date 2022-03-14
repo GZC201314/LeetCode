6a2aac65720a2a5e6f137c3c8aaab0eba44baf50
GZC201314 <1739084007@qq.com>
2022-03-14 22:29:16 +0800

    599. 两个列表的最小索引总和

--- src/main/java/org/gzc/leetcode/Solution202203.java
+++ src/main/java/org/gzc/leetcode/Solution202203.java
@@ -19,7 +19,7 @@ public class Solution202203 {
     int n;
     List<Integer>[] children;
 
-    public static void main(String[] args) {
+    public void main(String[] args) {
         Scanner input = new Scanner(System.in);
         int qusetionNum = input.nextInt();
         switch (qusetionNum) {
@@ -37,7 +37,13 @@ public class Solution202203 {
                 System.out.println(preorder1(new Node()));
                 break;
             case 393:
-                System.out.println(validUtf8(new int[]{1,2,3}));
+                System.out.println(validUtf8(new int[]{1, 2, 3}));
+                break;
+            case 599:
+                System.out.println(Arrays.toString(findRestaurant(new String[]{"1", "2", "3"}, new String[]{"1", "2", "3"})));
+                break;
+            case 2049:
+                System.out.println(countHighestScoreNodes(new int[]{1,2,3}));
                 break;
             default:
                 break;
@@ -216,6 +222,34 @@ public class Solution202203 {
             cnt = 1;
         }
         return n - size;
+    }
+
+    /**
+     * 599. 两个列表的最小索引总和
+     * @param list1 第一个列表
+     * @param list2 第二个列表
+     * @return 最小索引总和
+     */
+    public String[] findRestaurant(String[] list1, String[] list2) {
+        Map<String, Integer> index = new HashMap<>();
+        for (int i = 0; i < list1.length; i++) {
+            index.put(list1[i], i);
+        }
+        List<String> ret = new ArrayList<>();
+        int indexSum = Integer.MAX_VALUE;
+        for (int i = 0; i < list2.length; i++) {
+            if (index.containsKey(list2[i])) {
+                int j = index.get(list2[i]);
+                if (i + j < indexSum) {
+                    ret.clear();
+                    ret.add(list2[i]);
+                    indexSum = i + j;
+                } else if (i + j == indexSum) {
+                    ret.add(list2[i]);
+                }
+            }
+        }
+        return ret.toArray(new String[0]);
     }
 
 }
