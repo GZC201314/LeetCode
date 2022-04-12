24402f6c39f509c7808afb99d1f3907675294620
GZC201314 <1739084007@qq.com>
2022-04-12 20:08:05 +0800

    806. 写字符串需要的行数

--- src/main/java/org/gzc/leetcode/Solution202204.java
+++ src/main/java/org/gzc/leetcode/Solution202204.java
@@ -13,7 +13,7 @@ public class Solution202204 {
         int qusetionNum = input.nextInt();
         switch (qusetionNum) {
             case 954:
-                System.out.println(canReorderDoubled(new int[]{3,1,3,6}));
+                System.out.println(canReorderDoubled(new int[]{3, 1, 3, 6}));
                 break;
             default:
                 break;
@@ -30,7 +30,7 @@ public class Solution202204 {
     public static boolean canReorderDoubled(int[] arr) {
         Map<Integer, Integer> cnt = new HashMap<>();
         for (int num : arr) {
-            cnt.put(num, cnt.getOrDefault(num, 0)+1);
+            cnt.put(num, cnt.getOrDefault(num, 0) + 1);
         }
         if (cnt.getOrDefault(0, 0) % 2 != 0) {
             return false;
@@ -39,11 +39,41 @@ public class Solution202204 {
         absKey.sort(Comparator.comparingInt(Math::abs));
 
         for (int key : absKey) {
-            if (cnt.getOrDefault(2 * key,0) < cnt.get(key)) {
+            if (cnt.getOrDefault(2 * key, 0) < cnt.get(key)) {
                 return false;
             }
             cnt.put(2 * key, cnt.getOrDefault(2 * key, 0) - cnt.get(key));
         }
         return true;
     }
+
+    /**
+     * 806. 写字符串需要的行数
+     */
+    public int[] numberOfLines(int[] widths, String s) {
+
+        char[] chars = s.toCharArray();
+        int line = 0;
+        int danwei = 0;
+        for (char ch : chars) {
+            int index = ch - 'a';
+            if (danwei + widths[index] < 100) {
+                danwei += widths[index];
+            } else if (danwei + widths[index] == 100) {
+                danwei = 0;
+                line++;
+            }else {
+                danwei = widths[index];
+                line++;
+            }
+        }
+        if(danwei ==0){
+            danwei =100;
+        }else {
+            line++;
+        }
+        return new int[]{line,danwei};
+
+    }
+
 }
