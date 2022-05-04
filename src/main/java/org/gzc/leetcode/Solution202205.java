c62c76aa48f8ae42dca0a0bf95c623d8734fb988
GZC201314 <1739084007@qq.com>
2022-05-04 23:44:51 +0800

    1823. 找出游戏的获胜者

--- src/main/java/org/gzc/leetcode/Solution202205.java
+++ src/main/java/org/gzc/leetcode/Solution202205.java
@@ -1,10 +1,14 @@
 package org.gzc.leetcode;
 
 
+import com.sun.tools.javac.util.Assert;
 import org.gzc.leetcode.model.Pair;
 
+import java.util.ArrayDeque;
 import java.util.Arrays;
+import java.util.Queue;
 import java.util.Scanner;
+import java.util.function.IntFunction;
 
 /**
  * @author GZC
@@ -17,9 +21,12 @@ public class Solution202205 {
         int qusetionNum = input.nextInt();
         switch (qusetionNum) {
             case 937:
-                System.out.println(Arrays.toString(reorderLogFiles(new String[]{"a1 9 2 3 1","g1 act car","zo4 4 7","ab1 off key dog","a8 act zoo","a2 act car"})));
+                System.out.println(Arrays.toString(reorderLogFiles(new String[]{"a1 9 2 3 1", "g1 act car", "zo4 4 7", "ab1 off key dog", "a8 act zoo", "a2 act car"})));
                 break;
 
+            case 1823:
+                System.out.println(findTheWinner(5,2));
+                System.out.println(findTheWinner1(5,2));
             default:
                 break;
         }
@@ -41,9 +48,9 @@ public class Solution202205 {
             String log0 = pair0.getLog();
             int logIndex0 = pair0.getIndex();
             String log1 = pair1.getLog();
-            int logIndex1 =  pair1.getIndex();
-            String log0SubString = log0.substring(log0.indexOf(" ")+1);
-            String log1SubString = log1.substring(log1.indexOf(" ")+1);
+            int logIndex1 = pair1.getIndex();
+            String log0SubString = log0.substring(log0.indexOf(" ") + 1);
+            String log1SubString = log1.substring(log1.indexOf(" ") + 1);
             boolean log0IsDigit = Character.isDigit(log0SubString.charAt(0));
             boolean log1IsDigit = Character.isDigit(log1SubString.charAt(0));
             if (log0IsDigit) {
@@ -58,7 +65,7 @@ public class Solution202205 {
                 } else {
                     /*如果两个都是字符传,在内容不同时，忽略标识符后，按内容字母顺序排序；在内容相同时，按标识符排序。*/
                     int i = log0SubString.compareTo(log1SubString);
-                    return i == 0 ? log0.substring(0,log0.indexOf(" ")).compareTo(log1.substring(0,log1.indexOf(" "))) : i;
+                    return i == 0 ? log0.substring(0, log0.indexOf(" ")).compareTo(log1.substring(0, log1.indexOf(" "))) : i;
                 }
             }
         });
@@ -66,6 +73,65 @@ public class Solution202205 {
             logs[i] = arr[i].getLog();
         }
         return logs;
+    }
+
+    /**
+     * 1823. 找出游戏的获胜者
+     */
+    public static int findTheWinner(int n, int k) {
+        int result =0;
+        int index =0;
+        int num =0;
+        int restChild = n;
+        int[] childs = new int[n];
+        while (restChild >1){
+            // 如果当前的用户没有被淘汰
+            if(childs[index] == 0){
+                //叫号
+                num++;
+                if(num == k){
+                    //号码清零
+                    num=0;
+                    //用户淘汰
+                    childs[index%n] = -1;
+                    index = (index+1)%n;
+                    //剩余用户-1
+                    restChild--;
+                }else {
+                    num = num%k;
+                    index = (index+1)%n;
+                }
+            }else { // 如果当前的用户被淘汰了
+
+                index = (index+1)%n;
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            if(childs[i] == 0){
+                result = i+1;
+            }
+        }
+        return result;
+    }
+
+    /**
+     * 1823. 找出游戏的获胜者
+     */
+    public static int findTheWinner1(int n, int k) {
+        Queue<Integer> queue = new ArrayDeque<>();
+        for (int i = 1; i <= n; i++) {
+            queue.offer(i);
+        }
+        while (queue.size() > 1) {
+            for (int i = 1; i < k; i++) {
+                queue.offer(queue.poll());
+            }
+            queue.poll();
+        }
+        if (queue.isEmpty()){
+            return 0;
+        }
+        return queue.peek();
     }
 
 
