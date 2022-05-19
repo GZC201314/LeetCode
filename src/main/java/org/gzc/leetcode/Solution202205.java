2bf2d38c3227c5cfc9491c5dc05f72bc4f95924a
GZC201314 <1739084007@qq.com>
2022-05-19 16:46:08 +0800

    462. 最少移动次数使数组元素相等 II

--- src/main/java/org/gzc/leetcode/Solution202205.java
+++ src/main/java/org/gzc/leetcode/Solution202205.java
@@ -22,6 +22,9 @@ public class Solution202205 {
                 TreeNode treeNode = inorderSuccessor(new TreeNode(2, new TreeNode(1), new TreeNode(3)), new TreeNode(1));
                 System.out.println(treeNode != null ? treeNode.val : null);
                 break;
+            case 462:
+                System.out.println(minMoves2(new int[]{1,10,2,9}));
+                break;
             case 713:
                 System.out.println(numSubarrayProductLessThanK(new int[]{10, 9, 10, 4, 3, 8, 3, 3, 6, 2, 10, 10, 9, 3}, 19));
                 break;
@@ -277,6 +280,20 @@ public class Solution202205 {
             }
         }
         return true;
+    }
+
+    /**
+     * 462. 最少移动次数使数组元素相等 II
+     * PS: target 取中位数的时候是移动次数最少的,至于为什么取中位数的证明,则是因为在最大值和最小值的时候,target在之间取任何值移动次数都是定值,不同的拆分直到取到中位数为止,发现取到的次数最少
+     */
+    public static int minMoves2(int[] nums) {
+        int result =0;
+        Arrays.sort(nums);
+        int avg = nums[nums.length/2];
+        for (int num:nums) {
+            result+=Math.abs(num-avg);
+        }
+        return result;
     }
 }
 
