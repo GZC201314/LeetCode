57468ff84c531f1c1f250f6095548e0dee45ac89
GZC201314 <1739084007@qq.com>
2022-08-10 22:23:45 +0800

    640. 求解方程

--- src/main/java/org/gzc/leetcode/Solution202208.java
+++ src/main/java/org/gzc/leetcode/Solution202208.java
@@ -6,6 +6,7 @@ import org.gzc.leetcode.model.Node;
 import java.util.*;
 import java.util.stream.Stream;
 
+
 /**
  * @author GZC
  */
@@ -16,22 +17,25 @@ public class Solution202208 {
         int questionNum = input.nextInt();
         switch (questionNum) {
             case 503:
-                System.out.println(Arrays.toString(nextGreaterElements(new int[]{1,2,3,4,3})));
+                System.out.println(Arrays.toString(nextGreaterElements(new int[]{1, 2, 3, 4, 3})));
                 break;
             case 1403:
                 System.out.println(minSubsequence(new int[]{4, 4, 6, 7, 7}));
                 break;
             case 623:
-                System.out.println(addOneRow(new TreeNode(4),1,3));
+                System.out.println(addOneRow(new TreeNode(4), 1, 3));
                 break;
             case 123:
-                morris(new Node(5,new Node(4),new Node(2)));
+                morris(new Node(5, new Node(4), new Node(2)));
                 break;
             case 124:
-                preOrderMorris(new Node(5,null,new Node(2)));
+                preOrderMorris(new Node(5, null, new Node(2)));
                 break;
             case 507:
                 System.out.println(checkPerfectNumber(28));
+                break;
+            case 640:
+                System.out.println(solveEquation("3x+6=2x"));
                 break;
             case 508:
                 System.out.println(Arrays.toString(findFrequentTreeSum(new TreeNode(5, new TreeNode(2), new TreeNode(-3)))));
@@ -49,12 +53,12 @@ public class Solution202208 {
 
         int n = nums.length;
         int[] result = new int[n];
-        Arrays.fill(result,-1);
+        Arrays.fill(result, -1);
         for (int i = 0; i < n; i++) {
             int num = nums[i];
             for (int j = i + 1; j < i + n; j++) {
-                if (nums[j%n] > num) {
-                    result[i] = nums[j%n];
+                if (nums[j % n] > num) {
+                    result[i] = nums[j % n];
                     break;
                 }
             }
@@ -71,15 +75,15 @@ public class Solution202208 {
         // 对数组进行排序
         Arrays.sort(nums);
         int left = 0;
-        int right = n-1;
+        int right = n - 1;
         int leftNum = 0;
         result.add(nums[right]);
         int rightNum = nums[right--];
 
-        while(left <= right){
-            if (leftNum+nums[left] < rightNum){
+        while (left <= right) {
+            if (leftNum + nums[left] < rightNum) {
                 leftNum += nums[left++];
-            }else {
+            } else {
                 result.add(nums[right]);
                 rightNum += nums[right--];
             }
@@ -91,33 +95,33 @@ public class Solution202208 {
      * 623. 在二叉树中增加一行
      */
     public static TreeNode addOneRow(TreeNode root, int val, int depth) {
-        if (depth == 1){
-            return new TreeNode(val,root,null);
+        if (depth == 1) {
+            return new TreeNode(val, root, null);
         }
         Queue<TreeNode> queue = new LinkedList<>();
         queue.add(root);
 
-        int curDepth =1;
-        while (!queue.isEmpty()){
+        int curDepth = 1;
+        while (!queue.isEmpty()) {
             int size = queue.size();
-            if (curDepth == depth-1){
+            if (curDepth == depth - 1) {
                 for (int i = 0; i < size; i++) {
                     TreeNode node = queue.poll();
                     assert node != null;
                     TreeNode left = node.left;
                     TreeNode right = node.right;
-                    node.left = new TreeNode(val,left,null);
-                    node.right = new TreeNode(val,null,right);
+                    node.left = new TreeNode(val, left, null);
+                    node.right = new TreeNode(val, null, right);
                 }
                 break;
             }
             for (int i = 0; i < size; i++) {
                 TreeNode node = queue.poll();
                 assert node != null;
-                if(node.left != null){
+                if (node.left != null) {
                     queue.offer(node.left);
                 }
-                if(node.right != null){
+                if (node.right != null) {
                     queue.offer(node.right);
                 }
             }
@@ -133,15 +137,15 @@ public class Solution202208 {
      */
     public static boolean checkPerfectNumber(int num) {
 
-        if(num ==1){
+        if (num == 1) {
             return false;
         }
-        int sum =1;
-        for (int i = 2; i*i <= num  ; i++) {
-            if(num%i ==0){
-                sum+=i;
-                if (i*i <= num){
-                    sum+=num/i;
+        int sum = 1;
+        for (int i = 2; i * i <= num; i++) {
+            if (num % i == 0) {
+                sum += i;
+                if (i * i <= num) {
+                    sum += num / i;
                 }
             }
         }
@@ -152,7 +156,7 @@ public class Solution202208 {
      * 508. 出现次数最多的子树元素和
      */
     public static int[] findFrequentTreeSum(TreeNode root) {
-        Map<TreeNode,Integer> sumMap = new HashMap<>();
+        Map<TreeNode, Integer> sumMap = new HashMap<>();
         Map<Integer, Integer> countMap = new HashMap<>();
         dfsCheckPerfectNumber(root, sumMap);
 
@@ -161,8 +165,8 @@ public class Solution202208 {
         int max = stream.max(Integer::compareTo).get();
         List<Integer> list = new ArrayList<>();
         Set<Map.Entry<Integer, Integer>> entries = countMap.entrySet();
-        entries.forEach((entry)->{
-            if (entry.getValue() == max){
+        entries.forEach((entry) -> {
+            if (entry.getValue() == max) {
                 list.add(entry.getKey());
             }
         });
@@ -176,44 +180,44 @@ public class Solution202208 {
 
     }
 
-    public static int dfsCheckPerfectNumber(TreeNode node, Map<TreeNode,Integer> sumMap){
-        if(node == null){
+    public static int dfsCheckPerfectNumber(TreeNode node, Map<TreeNode, Integer> sumMap) {
+        if (node == null) {
             return 0;
         }
-        if(node.left == null && node.right == null){
+        if (node.left == null && node.right == null) {
             sumMap.put(node, node.val);
             return node.val;
         }
-        if(sumMap.containsKey(node)){
+        if (sumMap.containsKey(node)) {
             return sumMap.get(node);
         }
-        int sum = node.val+dfsCheckPerfectNumber(node.left,sumMap)+dfsCheckPerfectNumber(node.right,sumMap);
-        sumMap.put(node,sum);
+        int sum = node.val + dfsCheckPerfectNumber(node.left, sumMap) + dfsCheckPerfectNumber(node.right, sumMap);
+        sumMap.put(node, sum);
         return sum;
     }
 
     /**
      * 二叉树 morris 遍历
      */
-    public static void morris(Node head){
-        if(head == null){
+    public static void morris(Node head) {
+        if (head == null) {
             return;
         }
         Node cur = head;
         Node mostRight;
-        while(cur != null){
+        while (cur != null) {
             // mostRight 变成了cur左子树的最右的节点
             mostRight = cur.left;
-            if(mostRight != null){
-                while (mostRight.right != null && mostRight.right !=cur){
+            if (mostRight != null) {
+                while (mostRight.right != null && mostRight.right != cur) {
                     mostRight = mostRight.right;
                 }
                 // 第一次访问cur
-                if(mostRight.right == null){
+                if (mostRight.right == null) {
                     mostRight.right = cur;
                     cur = cur.left;
                     continue;
-                }else {
+                } else {
                     //第二次访问cur
                     mostRight.right = null;
                 }
@@ -225,34 +229,83 @@ public class Solution202208 {
     /**
      * mirris 先序遍历
      */
-    public static void preOrderMorris(Node head){
-        if(head == null){
+    public static void preOrderMorris(Node head) {
+        if (head == null) {
             return;
         }
         Node cur = head;
         Node mostRight;
-        while(cur != null){
+        while (cur != null) {
             // mostRight 变成了cur左子树的最右的节点
             mostRight = cur.left;
-            if(mostRight != null){
-                while (mostRight.right != null && mostRight.right !=cur){
+            if (mostRight != null) {
+                while (mostRight.right != null && mostRight.right != cur) {
                     mostRight = mostRight.right;
                 }
                 // 第一次访问cur
-                if(mostRight.right == null){
+                if (mostRight.right == null) {
                     System.out.println(cur.val);
                     mostRight.right = cur;
                     cur = cur.left;
                     continue;
-                }else {
+                } else {
                     //第二次访问cur
                     mostRight.right = null;
                 }
-            }else {
+            } else {
                 System.out.println(cur.val);
             }
             cur = cur.right;
         }
     }
+
+        /**
+         * 640. 求解方程
+         */
+        private static final String CHARS = "-+x=";
+        public static  String solveEquation(String equation) {
+            equation += "=";
+            int sign = 1, cur = 0, num = 0, k = 0;
+            boolean hasVal = false, left = true;
+            for (int i = 0; i < equation.length(); i++) {
+                char c = equation.charAt(i);
+                if (CHARS.contains(String.valueOf(c))) {
+                    if (c == 'x') {
+                        if (!hasVal && cur == 0) {
+                            cur = 1;
+                        }
+                        k += left ? sign * cur : -sign * cur;
+                    } else {
+                        num -= left ? sign * cur: -sign * cur;
+                    }
+                    cur = 0;
+                    hasVal = false;
+                }
+
+                switch (c) {
+                    case '-':
+                        sign = -1;
+                        break;
+                    case '+':
+                        sign = 1;
+                        break;
+                    case '=':
+                        sign = 1;
+                        left = false;
+                        break;
+                    case 'x':
+                        break;
+                    default:
+                        cur = cur * 10 + (c - '0');
+                        hasVal = true;
+                }
+
+            }
+            if (k == 0) {
+                return num != 0 ? "No solution" : "Infinite solutions";
+            }
+            return num % k == 0 ? String.format("x=%d", num/k) : "No solution";
+        }
+
 
 }
