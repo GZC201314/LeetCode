4c40f9ecf348dfe66a6571eaead595210b6e2f85
GZC <gzc201314@163.com>
2022-08-19 16:51:10 +0800

    2344. 使数组可以被整除的最少删除次数

--- src/main/java/org/gzc/leetcode/Solution202208.java
+++ src/main/java/org/gzc/leetcode/Solution202208.java
@@ -6,23 +6,20 @@ import org.gzc.leetcode.model.Node;
 import java.util.*;
 import java.util.stream.Stream;
 
-
 /**
  * @author GZC
  */
 public class Solution202208 {
-
-
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         int questionNum = input.nextInt();
         switch (questionNum) {
             case 503:
-                System.out.println(Arrays.toString(nextGreaterElements(new int[]{1, 2, 3, 4, 3})));
+                System.out.println(Arrays.toString(nextGreaterElements(new int[] {1, 2, 3, 4, 3})));
                 break;
             case 1403:
-                System.out.println(minSubsequence(new int[]{4, 4, 6, 7, 7}));
+                System.out.println(minSubsequence(new int[] {4, 4, 6, 7, 7}));
                 break;
             case 623:
                 System.out.println(addOneRow(new TreeNode(4), 1, 3));
@@ -37,10 +34,10 @@ public class Solution202208 {
                 System.out.println(checkPerfectNumber(28));
                 break;
             case 517:
-                System.out.println(findMinMoves(new int[]{3,4,5}));
+                System.out.println(findMinMoves(new int[] {3, 4, 5}));
                 break;
             case 666:
-                goodNote(new int[]{3,4,5});
+                goodNote(new int[] {3, 4, 5});
                 break;
             case 640:
                 System.out.println(solveEquation("3x+6=2x"));
@@ -49,14 +46,18 @@ public class Solution202208 {
                 System.out.println(deepestLeavesSum(new TreeNode(1)));
                 break;
             case 33:
-                System.out.println(verifyPostorder(new int[]{4, 6, 7, 5}));
+                System.out.println(verifyPostorder(new int[] {4, 6, 7, 5}));
                 break;
             case 1450:
-                System.out.println(busyStudent(new int[]{4, 6, 7, 5},new int[]{4, 6, 7, 5},4));
+                System.out.println(busyStudent(new int[] {4, 6, 7, 5}, new int[] {4, 6, 7, 5}, 4));
+                break;
+            case 2344:
+                System.out.println(minOperations(new int[] {4, 6, 7, 5}, new int[] {4, 6, 7, 5}));
                 break;
 
             case 508:
-                System.out.println(Arrays.toString(findFrequentTreeSum(new TreeNode(5, new TreeNode(2), new TreeNode(-3)))));
+                System.out
+                    .println(Arrays.toString(findFrequentTreeSum(new TreeNode(5, new TreeNode(2), new TreeNode(-3)))));
                 break;
             default:
                 break;
@@ -64,6 +65,35 @@ public class Solution202208 {
 
     }
 
+    /**
+     * 2344. 使数组可以被整除的最少删除次数
+     */
+    public static int minOperations(int[] nums, int[] numsDivide) {
+        int g =0;
+        for (int j : numsDivide) {
+            g = gcd(g, j);
+        }
+        TreeMap<Integer,Integer> map = new TreeMap<>();
+
+        int result =0;
+        for (int num : nums) {
+            map.put(num,map.getOrDefault(num,0)+1);
+        }
+
+        Set<Integer> keys = map.keySet();
+        for (Integer key : keys) {
+            if(g%key ==0){
+                return result;
+            }else {
+                result+= map.get(key);
+            }
+        }
+        return -1;
+    }
+
+    public static int gcd(int a, int b) {
+        return a == 0 ? b : gcd(b % a, a);
+    }
 
     /**
      * 剑指 Offer 33. 二叉搜索树的后序遍历序列
@@ -72,61 +102,64 @@ public class Solution202208 {
         int n = postorder.length;
         int[] inorder = Arrays.copyOf(postorder, n);
         Arrays.sort(inorder);
-        return verifyPostorderHandle(postorder,inorder,0,n-1,0,n-1);
+        return verifyPostorderHandle(postorder, inorder, 0, n - 1, 0, n - 1);
 
     }
 
-    public static boolean verifyPostorderHandle(int[] postorder, int[] inorder,int posti,int postj,int ini,int inj){
-        if(posti == postj && ini == inj){
+    public static boolean verifyPostorderHandle(int[] postorder, int[] inorder, int posti, int postj, int ini,
+        int inj) {
+        if (posti == postj && ini == inj) {
             return postorder[posti] == inorder[ini];
         }
         // 遍历完毕 返回true
-        if(posti>postj || ini>inj){
+        if (posti > postj || ini > inj) {
             return true;
         }
 
-        //获取根结点
+        // 获取根结点
         int root = postorder[postj];
-        //获取根结点在中序遍历中的位置
-        int rootInorderIndex =-1;
-        for (int i = ini;i<=inj;i++){
-            if(inorder[i] == root){
+        // 获取根结点在中序遍历中的位置
+        int rootInorderIndex = -1;
+        for (int i = ini; i <= inj; i++) {
+            if (inorder[i] == root) {
                 rootInorderIndex = i;
                 break;
             }
         }
-        if(rootInorderIndex ==-1){
+        if (rootInorderIndex == -1) {
             return false;
         }
         int[] subPostOrderLeft;
-        if(posti > posti + rootInorderIndex - ini){
+        if (posti > posti + rootInorderIndex - ini) {
             return true;
-        }else {
+        } else {
             subPostOrderLeft = Arrays.copyOfRange(postorder, posti, posti + rootInorderIndex - ini);
         }
         int[] subPostOrderRight;
-        if(posti+rootInorderIndex-ini>postj){
+        if (posti + rootInorderIndex - ini > postj) {
             return true;
-        }else {
-            subPostOrderRight = Arrays.copyOfRange(postorder, posti+rootInorderIndex-ini, postj);
+        } else {
+            subPostOrderRight = Arrays.copyOfRange(postorder, posti + rootInorderIndex - ini, postj);
         }
         int leftMax = Integer.MIN_VALUE;
         int rightMin = Integer.MAX_VALUE;
 
-        if(subPostOrderLeft.length > 0){
+        if (subPostOrderLeft.length > 0) {
             for (int node : subPostOrderLeft) {
-                leftMax = Math.max(leftMax,node);
+                leftMax = Math.max(leftMax, node);
             }
         }
-        if(subPostOrderRight.length > 0){
+        if (subPostOrderRight.length > 0) {
             for (int node : subPostOrderRight) {
-                rightMin = Math.min(rightMin,node);
+                rightMin = Math.min(rightMin, node);
             }
         }
 
-
-
-        return verifyPostorderHandle(postorder,inorder,posti,posti+rootInorderIndex-ini-1,ini,rootInorderIndex-1) && verifyPostorderHandle(postorder,inorder,posti+rootInorderIndex-ini,postj-1,rootInorderIndex+1,inj) && root>leftMax && root<rightMin;
+        return verifyPostorderHandle(postorder, inorder, posti, posti + rootInorderIndex - ini - 1, ini,
+            rootInorderIndex - 1)
+            && verifyPostorderHandle(postorder, inorder, posti + rootInorderIndex - ini, postj - 1,
+                rootInorderIndex + 1, inj)
+            && root > leftMax && root < rightMin;
 
     }
 
@@ -135,27 +168,27 @@ public class Solution202208 {
      */
     public static int deepestLeavesSum(TreeNode root) {
 
-       return handleDeepestLeavesSum(root, 0).sum;
+        return handleDeepestLeavesSum(root, 0).sum;
     }
 
-    public static Info handleDeepestLeavesSum(TreeNode node, int deep){
-        if (node == null){
-            return new Info(deep,0);
+    public static Info handleDeepestLeavesSum(TreeNode node, int deep) {
+        if (node == null) {
+            return new Info(deep, 0);
         }
-        if(node.left ==null && node.right ==null){
+        if (node.left == null && node.right == null) {
             return new Info(deep, node.val);
         }
         Info leftInfo = handleDeepestLeavesSum(node.left, deep + 1);
         Info rightInfo = handleDeepestLeavesSum(node.right, deep + 1);
         int sum;
         int maxDeep = leftInfo.deep;
-        if (leftInfo.deep> rightInfo.deep){
+        if (leftInfo.deep > rightInfo.deep) {
             sum = leftInfo.sum;
-        }else if (leftInfo.deep< rightInfo.deep){
+        } else if (leftInfo.deep < rightInfo.deep) {
             maxDeep = rightInfo.deep;
             sum = rightInfo.sum;
-        }else {
-            sum = leftInfo.sum+ rightInfo.sum;
+        } else {
+            sum = leftInfo.sum + rightInfo.sum;
         }
         return new Info(maxDeep, sum);
     }
@@ -252,7 +285,6 @@ public class Solution202208 {
             curDepth++;
         }
 
-
         return root;
     }
 
@@ -288,7 +320,7 @@ public class Solution202208 {
         Stream<Integer> stream = countMap.values().stream();
         int max = 0;
         Optional<Integer> max1 = stream.max(Integer::compareTo);
-        if (max1.isPresent()){
+        if (max1.isPresent()) {
             max = max1.get();
         }
         List<Integer> list = new ArrayList<>();
@@ -347,7 +379,7 @@ public class Solution202208 {
                     cur = cur.left;
                     continue;
                 } else {
-                    //第二次访问cur
+                    // 第二次访问cur
                     mostRight.right = null;
                 }
             }
@@ -378,7 +410,7 @@ public class Solution202208 {
                     cur = cur.left;
                     continue;
                 } else {
-                    //第二次访问cur
+                    // 第二次访问cur
                     mostRight.right = null;
                 }
             } else {
@@ -388,53 +420,54 @@ public class Solution202208 {
         }
     }
 
-        /**
-         * 640. 求解方程
-         */
-        private static final String CHARS = "-+x=";
-        public static  String solveEquation(String equation) {
-            equation += "=";
-            int sign = 1, cur = 0, num = 0, k = 0;
-            boolean hasVal = false, left = true;
-            for (int i = 0; i < equation.length(); i++) {
-                char c = equation.charAt(i);
-                if (CHARS.contains(String.valueOf(c))) {
-                    if (c == 'x') {
-                        if (!hasVal && cur == 0) {
-                            cur = 1;
-                        }
-                        k += left ? sign * cur : -sign * cur;
-                    } else {
-                        num -= left ? sign * cur: -sign * cur;
+    /**
+     * 640. 求解方程
+     */
+    private static final String CHARS = "-+x=";
+
+    public static String solveEquation(String equation) {
+        equation += "=";
+        int sign = 1, cur = 0, num = 0, k = 0;
+        boolean hasVal = false, left = true;
+        for (int i = 0; i < equation.length(); i++) {
+            char c = equation.charAt(i);
+            if (CHARS.contains(String.valueOf(c))) {
+                if (c == 'x') {
+                    if (!hasVal && cur == 0) {
+                        cur = 1;
                     }
-                    cur = 0;
-                    hasVal = false;
+                    k += left ? sign * cur : -sign * cur;
+                } else {
+                    num -= left ? sign * cur : -sign * cur;
                 }
-
-                switch (c) {
-                    case '-':
-                        sign = -1;
-                        break;
-                    case '+':
-                        sign = 1;
-                        break;
-                    case '=':
-                        sign = 1;
-                        left = false;
-                        break;
-                    case 'x':
-                        break;
-                    default:
-                        cur = cur * 10 + (c - '0');
-                        hasVal = true;
-                }
-
+                cur = 0;
+                hasVal = false;
             }
-            if (k == 0) {
-                return num != 0 ? "No solution" : "Infinite solutions";
+
+            switch (c) {
+                case '-':
+                    sign = -1;
+                    break;
+                case '+':
+                    sign = 1;
+                    break;
+                case '=':
+                    sign = 1;
+                    left = false;
+                    break;
+                case 'x':
+                    break;
+                default:
+                    cur = cur * 10 + (c - '0');
+                    hasVal = true;
             }
-            return num % k == 0 ? String.format("x=%d", num/k) : "No solution";
+
         }
+        if (k == 0) {
+            return num != 0 ? "No solution" : "Infinite solutions";
+        }
+        return num % k == 0 ? String.format("x=%d", num / k) : "No solution";
+    }
 
     /**
      * 517. 超级洗衣机
@@ -445,21 +478,21 @@ public class Solution202208 {
         for (int machine : machines) {
             sum += machine;
         }
-        if(sum%n !=0){
+        if (sum % n != 0) {
             return -1;
         }
         int ans = Integer.MIN_VALUE;
-        int avg = sum/n;
-        int leftSum =0;
+        int avg = sum / n;
+        int leftSum = 0;
         for (int i = 0; i < n; i++) {
-            int leftRest = leftSum- i*avg;
-            int rightRest = sum -leftSum-machines[i] - (n-i-1)*avg;
-            if (leftRest<0 && rightRest<0){
+            int leftRest = leftSum - i * avg;
+            int rightRest = sum - leftSum - machines[i] - (n - i - 1) * avg;
+            if (leftRest < 0 && rightRest < 0) {
                 ans = Math.max(ans, Math.abs(leftRest) + Math.abs(rightRest));
-            }else {
-                ans = Math.max(ans,Math.max(Math.abs(leftRest),Math.abs(rightRest)));
+            } else {
+                ans = Math.max(ans, Math.max(Math.abs(leftRest), Math.abs(rightRest)));
             }
-            leftSum +=machines[i];
+            leftSum += machines[i];
         }
         return ans;
     }
@@ -467,29 +500,29 @@ public class Solution202208 {
     public static int[] dp;
     public static int[] used;
 
-        public static void goodNote(int[] nices){
-            int n = nices.length;
-            dp = new int[n+1];
-            used = new int[n];
-            System.out.println(handle(nices,0));
-            System.out.println(Arrays.toString(dp));
-        }
+    public static void goodNote(int[] nices) {
+        int n = nices.length;
+        dp = new int[n + 1];
+        used = new int[n];
+        System.out.println(handle(nices, 0));
+        System.out.println(Arrays.toString(dp));
+    }
 
-    public static int handle(int[] nums,int index){
-        if(index >= nums.length){
+    public static int handle(int[] nums, int index) {
+        if (index >= nums.length) {
             return 0;
         }
-        if(dp[index] !=0){
+        if (dp[index] != 0) {
             return dp[index];
         }
-        if(index == nums.length-1){
+        if (index == nums.length - 1) {
             return dp[index];
         }
-        if(index == nums.length-2){
-            dp[index] = Math.max(nums[index],nums[index+1]);
+        if (index == nums.length - 2) {
+            dp[index] = Math.max(nums[index], nums[index + 1]);
             return dp[index];
         }
-        dp[index] = Math.max(handle(nums,index+2)+nums[index],handle(nums,index+1));
+        dp[index] = Math.max(handle(nums, index + 2) + nums[index], handle(nums, index + 1));
         return dp[index];
     }
 
@@ -498,10 +531,10 @@ public class Solution202208 {
      */
     public static int busyStudent(int[] startTime, int[] endTime, int queryTime) {
 
-        int result =0;
+        int result = 0;
         int n = startTime.length;
         for (int i = 0; i < n; i++) {
-            if(startTime[i] <= queryTime && queryTime <= endTime[i]){
+            if (startTime[i] <= queryTime && queryTime <= endTime[i]) {
                 result++;
             }
         }
