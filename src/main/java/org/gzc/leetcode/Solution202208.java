550b66b9bc85cad3c7434e573bb256ab5fe1dfc1
GZC201314 <1739084007@qq.com>
2022-09-12 23:50:33 +0800

    1608. 特殊数组的特征值

--- src/main/java/org/gzc/leetcode/Solution202208.java
+++ src/main/java/org/gzc/leetcode/Solution202208.java
@@ -316,10 +316,12 @@ public class Solution202208 {
 
         @Override
         public boolean equals(Object o) {
-            if (this == o)
+            if (this == o) {
                 return true;
-            if (o == null || getClass() != o.getClass())
+            }
+            if (o == null || getClass() != o.getClass()) {
                 return false;
+            }
             CanCrossInfo info = (CanCrossInfo)o;
             return cur == info.cur && kStep == info.kStep;
         }
