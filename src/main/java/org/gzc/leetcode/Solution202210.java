870a650efd104a55a639dbf3bca99617d01c7e04
GZC <gzc201314@163.com>
2022-10-13 14:41:35 +0800

    1733. 需要教语言的最少人数

--- src/main/java/org/gzc/leetcode/Solution202210.java
+++ src/main/java/org/gzc/leetcode/Solution202210.java
@@ -42,6 +42,9 @@ public class Solution202210 {
             case 904:
                 System.out.println(totalFruit(new int[] {1, 2, 3, 2, 2}));
                 break;
+            case 1733:
+                System.out.println(minimumTeachings(2,new int[][] {{1},{2},{1,2}},new int[][]{{1,2},{1,3},{2,3}}));
+                break;
             case 957:
                 System.out.println(Arrays.toString(prisonAfterNDays(new int[] {0, 1, 0, 1, 1, 0, 0, 1}, 7)));
                 break;
@@ -64,6 +67,65 @@ public class Solution202210 {
             default:
                 break;
         }
+    }
+
+    /**
+     * 1733. 需要教语言的最少人数
+     */
+    public static int minimumTeachings(int n, int[][] languages, int[][] friendships) {
+        // 人数
+        int m = languages.length;
+        // 表示是否遍历过
+        boolean[] p = new boolean[m + 1];
+        // 统计每种语言下，可以说该门语言但不能直接和好友交流的人
+        HashSet<Integer>[] set = new HashSet[n + 1];
+        for (int i = 0; i < n + 1; i++) {
+            set[i] = new HashSet<>();
+        }
+        for (int[] friend : friendships) {
+            int u = friend[0];
+            int v = friend[1];
+            // 使用set判断u和v是否可以通过某种语言进行直接交流，是就跳过
+            Set<Integer> tmp = new HashSet<>();
+            boolean flag = false;
+            for (int k : languages[u - 1]) {
+                tmp.add(k);
+            }
+            for (int k : languages[v - 1]) {
+                if (tmp.contains(k)) {
+                    flag = true;
+                    break;
+                }
+            }
+            // 是，就跳过
+            if (flag)
+                continue;
+            // 对于u和v，如果没被遍历过，则加入对应语言set中
+            if (!p[u]) {
+                p[u] = true;
+                for (int k : languages[u - 1]) {
+                    set[k].add(u);
+                }
+            }
+            if (!p[v]) {
+                p[v] = true;
+                for (int k : languages[v - 1]) {
+                    set[k].add(v);
+                }
+            }
+        }
+        int ans = m;
+        // num，表示不能直接交流的人的总人数
+        int num = 0;
+        for (int i = 1; i < m + 1; i++) {
+            if (p[i])
+                num++;
+        }
+        // 判断每种语言下，需要学习的人数，取最小即答案
+        for (int i = 1; i < n + 1; i++) {
+            ans = Math.min(ans, num - set[i].size());
+        }
+        return ans;
     }
 
     /**
