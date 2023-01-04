7f49c985f95409f4f9e19396b1559ab40ea742d3
guozhichao <gzc201314@163.com>
2023-01-04 22:32:14 +0800

    1802. 有界数组中指定下标处的最大值

--- src/main/java/org/gzc/leetcode/Solution202301.java
+++ src/main/java/org/gzc/leetcode/Solution202301.java
@@ -16,10 +16,70 @@ public class Solution202301 {
             case 2042:
                 System.out.println(areNumbersAscending("sunset is at 7 50 pm overnight lows will be in the low 51 and 60 s"));
                 break;
+            case 1802:
+                System.out.println(maxValue(995610677, 934568761, 999009430));
+                break;
             default:
                 break;
         }
     }
+
+
+    /**
+     * 1802. 有界数组中指定下标处的最大值
+     */
+    public static int maxValue(int n, int index, int maxSum) {
+        int left = 0;
+        int right = maxSum;
+        int mid = left + (right - left) / 2;
+        int ans = 0;
+        while (left <= right) {
+            // 当最大值为mid时，判断是否满足条件
+            // 计算满足条件需要的最大最小值
+            // 左边的最小值 0 -- index-1
+            int leftIndex = index - 1;
+            int leftNum = Math.max(mid - 1, 1);
+            long leftSum = 0;
+            while (leftIndex >= 0) {
+                leftSum += leftNum;
+                if (leftNum > 1) {
+                    leftNum--;
+                } else {
+                    if (leftIndex > 0) {
+                        leftSum += leftIndex;
+                    }
+                    break;
+                }
+                leftIndex--;
+            }
+            int rightIndex = index + 1;
+            long rightSum = 0;
+            int rightNum = Math.max(mid - 1, 1);
+            while (rightIndex < n) {
+                rightSum += rightNum;
+                if (rightNum > 1) {
+                    rightNum--;
+                } else {
+                    if (rightNum < n - 1) {
+                        rightSum += (n - rightIndex - 1);
+                    }
+                    break;
+                }
+                rightIndex++;
+            }
+            if (leftSum + rightSum <= maxSum - mid) {
+                ans = Math.max(ans, mid);
+                left = mid + 1;
+                mid = left + (right - left) / 2;
+            } else {
+                right = mid - 1;
+                mid = left + (right - left) / 2;
+            }
+        }
+        return ans;
+
+    }
+
 
     /**
      * 2042. 检查橘子中的数字是否递增
