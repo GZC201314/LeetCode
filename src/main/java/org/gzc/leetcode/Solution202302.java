3e489033cd071fde490d42bec38bb1c90f15a833
GZC201314 <1739084007@qq.com>
2023-02-08 22:21:27 +0800

    1233. 删除子文件夹

--- src/main/java/org/gzc/leetcode/Solution202302.java
+++ src/main/java/org/gzc/leetcode/Solution202302.java
@@ -19,31 +19,55 @@ public class Solution202302 {
         switch (questionNum) {
             case 1604:
                 System.out.println(alertNames(new String[]{"daniel", "daniel", "daniel", "luis", "luis", "luis", "luis"}, new String[]{"10:00", "10:40", "11:00", "09:00", "11:00", "13:00", "15:00"}));
+                break;
+            case 1233:
+                System.out.println(removeSubfolders(new String[]{"/a/b/c", "/a/b/ca", "/a/b/d"}));
             default:
                 break;
         }
     }
 
+    /**
+     * 1233. 删除子文件夹
+     */
+    public static List<String> removeSubfolders(String[] folder) {
+        List<String> ans = new ArrayList<>();
+        Arrays.sort(folder, Comparator.comparingInt(String::length));
+        for (String s : folder) {
+
+            boolean flag = true;
+            for (String an : ans) {
+                if (s.startsWith(an) && '/' == s.charAt(an.length())) {
+                    flag = false;
+                    break;
+                }
+            }
+            if (flag) {
+                ans.add(s);
+            }
+        }
+        return ans;
+    }
 
     /**
      * 1604. 警告一小时内使用相同员工卡大于等于三次的人
      */
     public static List<String> alertNames(String[] keyName, String[] keyTime) {
         List<String> ans = new ArrayList<>();
-        Map<String,List<Integer>> mapSet = new HashMap<>(10);
+        Map<String, List<Integer>> mapSet = new HashMap<>(10);
         int length = keyName.length;
         for (int i = 0; i < length; i++) {
-            if (mapSet.containsKey(keyName[i])){
+            if (mapSet.containsKey(keyName[i])) {
                 String[] split = keyTime[i].split(":");
                 int hour = Integer.parseInt(split[0]);
                 int min = Integer.parseInt(split[1]);
-                mapSet.get(keyName[i]).add(hour*60+min);
-            }else {
+                mapSet.get(keyName[i]).add(hour * 60 + min);
+            } else {
                 List<Integer> treeSet = new ArrayList<>();
                 String[] split = keyTime[i].split(":");
                 int hour = Integer.parseInt(split[0]);
                 int min = Integer.parseInt(split[1]);
-                treeSet.add(hour*60+min);
+                treeSet.add(hour * 60 + min);
                 mapSet.put(keyName[i], treeSet);
             }
         }
@@ -51,16 +75,16 @@ public class Solution202302 {
         for (Map.Entry<String, List<Integer>> stringListEntry : entries1) {
             List<Integer> value = stringListEntry.getValue();
             int size = value.size();
-            if (size <3){
+            if (size < 3) {
                 continue;
             }
             value.sort(Integer::compareTo);
-            int left =0;
-            int right =0;
-            while (right<size){
-                if (value.get(right)-value.get(left)<=60){
+            int left = 0;
+            int right = 0;
+            while (right < size) {
+                if (value.get(right) - value.get(left) <= 60) {
                     right++;
-                }else {
+                } else {
                     left++;
                 }
                 if (right - left >= 3) {
