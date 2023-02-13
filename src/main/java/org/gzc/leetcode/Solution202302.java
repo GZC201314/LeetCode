2867b695ed564af881d965f46d9115aabb3cfb48
GZC201314 <1739084007@qq.com>
2023-02-13 22:49:55 +0800

    1234. 替换子串得到平衡字符串

--- src/main/java/org/gzc/leetcode/Solution202302.java
+++ src/main/java/org/gzc/leetcode/Solution202302.java
@@ -30,9 +30,64 @@ public class Solution202302 {
                 break;
             case 1233:
                 System.out.println(removeSubfolders(new String[]{"/a/b/c", "/a/b/ca", "/a/b/d"}));
+                break;
+            case 1234:
+                System.out.println(balancedString("QWER"));
             default:
                 break;
         }
+    }
+
+
+    /**
+     * 1234. 替换子串得到平衡字符串
+     */
+    public static int balancedString(String s) {
+        // 使用数组来存储四个字符的出现次数（使用数组便于代码书写）
+        int[] counts = new int[26];
+        // 字符串长度
+        int len = s.length();
+        // n / 4
+        int limit = len / 4;
+        char ch;
+        // 初始化不替换内容字符出现次数数组，即初始滑动窗口维护一个空串
+        for(int i = 0; i < len; i++){
+            ch = s.charAt(i);
+            counts[ch - 'A']++;
+        }
+        // 初始化滑动窗口左右指针，维护的子串是[left,right]的内容
+        // 初始化子串为空，因此left=0，right=-1表示一个空子串
+        int left = 0;
+        int right = -1;
+        // 最小替换子串长度，初始为整个字符串长度
+        int minLength = len;
+        // 滑动窗口
+        while(left < len){
+            // 校验通过
+            if(check(counts, limit)){
+                // 记录当前合法子串的长度并更新最小长度
+                // 左指针右移，那么原本左指针指向的字符就变成不替换的内容，不替换内容多了一个字符，对应count数组中的值加1
+                minLength = Math.min(minLength, right - left + 1);
+                counts[s.charAt(left++) - 'A']++;
+            }else if(right < len - 1){
+                // 当前子串不合法且右指针还没到头
+                // 右指针右移，移动后的右指针指向的字符变成了子串的内容，不替换的内容少了一个字符，对应count数组中的值减1
+                counts[s.charAt(++right) - 'A']--;
+            }else{
+                // 右指针到头，搜索结束
+                break;
+            }
+        }
+        return minLength;
+
+    }
+
+    /**
+     * 校验函数，校验当前counts中四个字符的出现次数是否都小于等于limit；
+     * 是返回true，否返回false
+     */
+    private static boolean check(int[] counts, int limit){
+        return counts['Q' - 'A'] <= limit && counts['W' - 'A'] <= limit && counts['E' - 'A'] <= limit && counts['R' - 'A'] <= limit;
     }
 
     /**
