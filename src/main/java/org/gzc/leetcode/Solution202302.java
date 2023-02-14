7fefac72bd6ecac8d68468d55c29a8c601d8fe12
GZC201314 <1739084007@qq.com>
2023-02-14 22:56:37 +0800

    1124. Maximum period of good performance

--- src/main/java/org/gzc/leetcode/Solution202302.java
+++ src/main/java/org/gzc/leetcode/Solution202302.java
@@ -21,8 +21,8 @@ public class Solution202302 {
         switch (questionNum) {
             case 1797:
                 AuthenticationManager authenticationManager = new AuthenticationManager(10);
-                authenticationManager.generate("aaa",10);
-                authenticationManager.renew("bbb",12);
+                authenticationManager.generate("aaa", 10);
+                authenticationManager.renew("bbb", 12);
                 System.out.println(authenticationManager.countUnexpiredTokens(12));
                 break;
             case 1604:
@@ -33,11 +33,38 @@ public class Solution202302 {
                 break;
             case 1234:
                 System.out.println(balancedString("QWER"));
+                break;
+            case 1124:
+                System.out.println(longestwpi(new int[]{9, 9, 6, 0, 6, 6, 9}));
+                break;
             default:
                 break;
         }
     }
 
+
+    /**
+     * 1124. 表现良好的最长时间段
+     */
+    public static int longestwpi(int[] hours) {
+        int n = hours.length;
+        Map<Integer, Integer> map = new HashMap<>(64);
+        int s = 0, res = 0;
+        for (int i = 0; i < n; i++) {
+            s += hours[i] > 8 ? 1 : -1;
+            if (s > 0) {
+                res = Math.max(res, i + 1);
+            } else {
+                if (map.containsKey(s - 1)) {
+                    res = Math.max(res, i - map.get(s - 1));
+                }
+            }
+            if (!map.containsKey(s)) {
+                map.put(s, i);
+            }
+        }
+        return res;
+    }
 
     /**
      * 1234. 替换子串得到平衡字符串
@@ -51,7 +78,7 @@ public class Solution202302 {
         int limit = len / 4;
         char ch;
         // 初始化不替换内容字符出现次数数组，即初始滑动窗口维护一个空串
-        for(int i = 0; i < len; i++){
+        for (int i = 0; i < len; i++) {
             ch = s.charAt(i);
             counts[ch - 'A']++;
         }
@@ -62,18 +89,18 @@ public class Solution202302 {
         // 最小替换子串长度，初始为整个字符串长度
         int minLength = len;
         // 滑动窗口
-        while(left < len){
+        while (left < len) {
             // 校验通过
-            if(check(counts, limit)){
+            if (check(counts, limit)) {
                 // 记录当前合法子串的长度并更新最小长度
                 // 左指针右移，那么原本左指针指向的字符就变成不替换的内容，不替换内容多了一个字符，对应count数组中的值加1
                 minLength = Math.min(minLength, right - left + 1);
                 counts[s.charAt(left++) - 'A']++;
-            }else if(right < len - 1){
+            } else if (right < len - 1) {
                 // 当前子串不合法且右指针还没到头
                 // 右指针右移，移动后的右指针指向的字符变成了子串的内容，不替换的内容少了一个字符，对应count数组中的值减1
                 counts[s.charAt(++right) - 'A']--;
-            }else{
+            } else {
                 // 右指针到头，搜索结束
                 break;
             }
@@ -86,7 +113,7 @@ public class Solution202302 {
      * 校验函数，校验当前counts中四个字符的出现次数是否都小于等于limit；
      * 是返回true，否返回false
      */
-    private static boolean check(int[] counts, int limit){
+    private static boolean check(int[] counts, int limit) {
         return counts['Q' - 'A'] <= limit && counts['W' - 'A'] <= limit && counts['E' - 'A'] <= limit && counts['R' - 'A'] <= limit;
     }
 
