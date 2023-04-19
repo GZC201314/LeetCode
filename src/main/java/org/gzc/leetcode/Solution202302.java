b3ad42e65f93db7659f9bdc50770bcbf3b838a62
GZC201314 <1739084007@qq.com>
2023-04-19 23:21:15 +0800

    1043. 分隔数组以得到最大和

--- src/main/java/org/gzc/leetcode/Solution202302.java
+++ src/main/java/org/gzc/leetcode/Solution202302.java
@@ -119,6 +119,9 @@ public class Solution202302 {
             case 70:
                 System.out.println(singleNonDuplicate(new int[]{1,2,2,3,3,4,4}));
                 break;
+            case 1043:
+                System.out.println(maxSumAfterPartitioning(new int[]{1,2,2,3,3,4,4},3));
+                break;
             case 1026:
                 System.out.println(maxAncestorDiff(new TreeNode(3)));
                 break;
@@ -131,6 +134,27 @@ public class Solution202302 {
             default:
                 break;
         }
+    }
+
+
+    /**
+     * 1043. 分隔数组以得到最大和
+     */
+
+    public static int maxSumAfterPartitioning(int[] arr, int k) {
+        int[] dp = new int[arr.length];
+        dp[0] = arr[0];
+        for(int i = 1; i<k ;i ++){
+            dp[i] = arr[i] > dp[i-1]/i ? arr[i] * (i+1) : dp[i-1]/i*(i+1);
+        }
+        for(int i = k; i<arr.length; i++){
+            int max = arr[i];
+            for(int j = i; j>i-k; j--){
+                max = Math.max(max,arr[j]);
+                dp[i] = Math.max(dp[i],dp[j-1]+max*(i-j+1));
+            }
+        }
+        return dp[arr.length-1];
     }
 
     /**
