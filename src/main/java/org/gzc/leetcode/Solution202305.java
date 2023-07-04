bad591da5f8e7ca52dee2ebec77742f42a083b31
GZC201314 <1739084007@qq.com>
2023-07-04 22:41:37 +0800

    2679.矩阵中的和

--- src/main/java/org/gzc/leetcode/Solution202305.java
+++ src/main/java/org/gzc/leetcode/Solution202305.java
@@ -1,8 +1,7 @@
 package org.gzc.leetcode;
 
 import java.text.ParseException;
-import java.util.Arrays;
-import java.util.Scanner;
+import java.util.*;
 
 /**
  * @author GZC
@@ -17,7 +16,10 @@ public class Solution202305 {
                 System.out.println(Arrays.toString(applyOperations(new int[]{1, 2, 3})));
                 break;
             case 2461:
-                System.out.println(Arrays.toString(applyOperations(new int[]{1, 2, 3}))+" ");
+                System.out.println(Arrays.toString(applyOperations(new int[]{1, 2, 3})) + " ");
+                break;
+            case 2679:
+                System.out.println(matrixSum(new int[][]{{7, 2, 1}, {6, 4, 2}, {6, 5, 3}, {3, 2, 1}}));
                 break;
             default:
                 break;
@@ -33,15 +35,40 @@ public class Solution202305 {
         for (int i = 0; i < n - 1; i++) {
             if (nums[i] == nums[i + 1]) {
                 nums[i] *= nums[i];
-                nums[i+1] =0;
+                nums[i + 1] = 0;
             }
         }
         int index = 0;
         int[] ans = new int[n];
         for (int num : nums) {
-            if (num !=0){
+            if (num != 0) {
                 ans[index++] = num;
             }
+        }
+        return ans;
+    }
+
+    /**
+     * 2679.矩阵中的和
+     */
+    public static int matrixSum(int[][] nums) {
+        int n = nums[0].length;
+        int m = nums.length;
+        List<PriorityQueue<Integer>> priorityQueueList = new ArrayList<>();
+        for (int i = 0; i < m; i++) {
+            PriorityQueue<Integer> priorityQueue = new PriorityQueue<>((o1, o2) -> o2 - o1);
+            for (int num : nums[i]) {
+                priorityQueue.offer(num);
+            }
+            priorityQueueList.add(priorityQueue);
+        }
+        int ans = 0;
+        for (int i = 0; i < n; i++) {
+            int max = 0;
+            for (PriorityQueue<Integer> priorityQueue : priorityQueueList) {
+                max = Math.max(max, priorityQueue.poll());
+            }
+            ans += max;
         }
         return ans;
     }
