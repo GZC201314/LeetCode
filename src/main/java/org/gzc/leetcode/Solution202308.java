9fb5ac3e804d59e823199fe1237e56f649444875
guozc <guozc@bsfit.com.cn>
2023-11-01 20:51:39 +0800

    feat: 删除错误的代码

--- src/main/java/org/gzc/leetcode/Solution202308.java
+++ src/main/java/org/gzc/leetcode/Solution202308.java
@@ -16,9 +16,6 @@ public class Solution202308 {
         Scanner input = new Scanner(System.in);
         int questionNum = input.nextInt();
         switch (questionNum) {
-            case 722:
-                log.info(String.valueOf(removeComments(new String[]{"/*Test program */", "int main()", "{ ", "  // variable declaration ", "int a, b, c;", "/* This is a test", "   multiline  ", "   comment for ", "   testing */", "a = b + c;", "}"})));
-                break;
             case 1749:
                 log.info(String.valueOf(maxAbsoluteSum(new int[]{2, -5, 1, -4, 3, -2})));
                 break;
@@ -419,7 +416,7 @@ public class Solution202308 {
         char[] chars = s.toCharArray();
         List<String> strArr = new ArrayList<>();
         for (char aChar : chars) {
-            strArr.add(aChar + "");
+            strArr.add(String.valueOf(aChar));
         }
         for (int i = n - 1; i >= 0; i--) {
             int index = indices[i];
@@ -445,7 +442,7 @@ public class Solution202308 {
                 char[] tarArr = target.toCharArray();
                 int tarLen = tarArr.length;
                 for (int j = tarLen - 1; j >= 0; j--) {
-                    strArr.add(indexOld, tarArr[j] + "");
+                    strArr.add(indexOld, String.valueOf(tarArr[j]));
                 }
             }
         }
@@ -517,95 +514,6 @@ public class Solution202308 {
         return curMul - curSum;
     }
 
-    /**
-     * 722. 删除注释
-     *
-     * @param source 源代码
-     * @return 去除注释的代码
-     */
-    public static List<String> removeComments(String[] source) {
-
-        List<String> ans = new ArrayList<>();
-        boolean flag = false;
-        for (String code : source) {
-            flag = isFlag(ans, flag, code);
-        }
-        return ans;
-    }
-
-    private static boolean isFlag(List<String> ans, boolean flag, String code) {
-        int multiIndex = code.indexOf("/*");
-        int multiIndex1 = code.indexOf("*/");
-        int singleIndex = code.indexOf("//");
-        if (flag) {
-            //存在多行注释的结尾
-            if (multiIndex1 != -1) {
-                code = code.substring(multiIndex1 + 2).trim();
-                if (!"".equals(code)) {
-                    flag = isFlag(ans, false, code);
-                } else {
-                    flag = false;
-                }
-            }
-        } else {
-            if (singleIndex != -1) {
-                // 只有单行注释
-                if (multiIndex == -1) {
-                    code = code.substring(0, singleIndex).trim();
-                    if (!"".equals(code)) {
-                        ans.add(code);
-                    }
-                } else {
-                    //存在单行注释和多行注释,单行注释在前面
-                    if (singleIndex < multiIndex) {
-                        code = code.substring(0, singleIndex);
-                        if (!"".equals(code)) {
-                            ans.add(code);
-                        }
-                    } else {
-                        //存在单行注释和多行注释,多行注释在前面
-                        // 存在多行注释的结尾
-                        if (multiIndex1 != -1) {
-                            // /**/XX//XX
-                            if (singleIndex > multiIndex1) {
-                                code = code.substring(multiIndex1 + 2).trim();
-                                // XX//XX
-                                flag = isFlag(ans, flag, code);
-                            }
-                        } else {
-                            //存在单行注释和多行注释,多行注释在前面
-                            //不存在多行注释的结尾
-                            // /*XX//XX
-                            code = code.substring(0, multiIndex).trim();
-                            isFlag(ans, false, code);
-                            flag = true;
-                        }
-                    }
-                }
-            } else if (multiIndex != -1) {
-                //只有多行注释
-                // 存在多行注释的结尾
-                if (multiIndex1 != -1) {
-                    // /**/XXXX
-                    code = code.substring(multiIndex1 + 2).trim();
-                    // XX//XX
-                    flag = isFlag(ans, flag, code);
-                } else {
-                    //存在单行注释和多行注释,多行注释在前面
-                    //不存在多行注释的结尾
-                    // /*XX//XX
-                    code = code.substring(0, multiIndex).trim();
-                    isFlag(ans, false, code);
-                    flag = true;
-                }
-            } else {
-                if (!"".equals(code)) {
-                    ans.add(code);
-                }
-            }
-        }
-        return flag;
-    }
 
     /**
      * 1749. 任意子数组和的绝对值的最大值
